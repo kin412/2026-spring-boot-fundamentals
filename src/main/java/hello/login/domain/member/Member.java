9482dd17f44f4f3790c4cf74f15c1a5ccc36b196
MINDEV\kin41 <kin412@naver.com>
2026-02-08 02:44:21 +0900

    쿠키, 세션

---
+++ src/main/java/hello/login/domain/member/Member.java
@@ -0,0 +1,21 @@
+package hello.login.domain.member;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class Member {
+
+    private Long id; // 키
+
+    @NotEmpty
+    private String loginId; //로그인ID
+
+    @NotEmpty
+    private String name; // 사용자 이름
+
+    @NotEmpty
+    private String password; //비밀번호
+
+}
